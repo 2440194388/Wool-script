fcb11428b002114aa614fe7599090d9d45cec954
2440194388 <75823897+2440194388@users.noreply.github.com>
2021-03-05 22:55:35 +0800

    Update huoshan.js

--- huoshan.js
+++ huoshan.js
@@ -125,12 +125,12 @@ if (process.env.PLAYURL && process.env.PLAYURL.indexOf('#') > -1) {
     console.log(`============ 脚本执行-国际标准时间(UTC)：${new Date().toLocaleString()}  =============\n`)
     console.log(`============ 脚本执行-北京时间(UTC+8)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}  =============\n`)
  } else {
-    hsurlArr.push($.getdata('hsurl'))
-    hsheaderArr.push($.getdata('hsheader'))
-    hsbodyArr.push($.getdata('hsbody'))
-    playurlArr.push($.getdata('playurl'))
-    playheaderArr.push($.getdata('playheader'))
-    playbodyArr.push($.getdata('playbody'))
+    hsurlArr.push(hsurl)
+    hsheaderArr.push(hsheader)
+    hsbodyArr.push(hsbody)
+    playurlArr.push(playurl)
+    playheaderArr.push(playheader)
+    playbodyArr.push(playbody)
     let hscount = ($.getval('hscount') || '1');
   for (let i = 2; i <= hscount; i++) {
 	hsurlArr.push($.getdata(`hsurl${i}`))
