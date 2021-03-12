6df83b47e5fd80b766c36c7daf13c1347606e238
2440194388 <75823897+2440194388@users.noreply.github.com>
2021-03-12 21:14:45 +0800

    Add files via upload

--- huoshan.js
+++ huoshan.js
@@ -121,7 +121,13 @@ if (process.env.PLAYURL && process.env.PLAYURL.indexOf('#') > -1) {
   };
     console.log(`============ 脚本执行-国际标准时间(UTC)：${new Date().toLocaleString()}  =============\n`)
     console.log(`============ 脚本执行-北京时间(UTC+8)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}  =============\n`)
- } else {
+    hsurlArr.push(hsurl)
+    hsheaderArr.push(hsheader)
+    hsbodyArr.push(hsbody)
+    playurlArr.push(playurl)
+    playheaderArr.push(playheader)
+    playbodyArr.push(playbody)
+  } else {
     hsurlArr.push($.getdata('hsurl'))
     hsheaderArr.push($.getdata('hsheader'))
     hsbodyArr.push($.getdata('hsbody'))
