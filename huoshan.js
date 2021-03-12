65747519ec09a2d31303ce7a20d20becad6a50e0
2440194388 <75823897+2440194388@users.noreply.github.com>
2021-03-12 21:41:28 +0800

    Add files via upload

--- huoshan.js
+++ huoshan.js
@@ -155,7 +155,7 @@ if (!hsheaderArr[0] && !hsbodyArr[0] && !hsurlArr[0]) {
       message = ''
       note = ''
       hsurl = hsurlArr[i];
-      hsheader = hsheaderArr[i];
+      var hsheader = hsheaderArr[i];
       hsbody = hsbodyArr[i];
       playurl = playurlArr[i];
       playheader = playheaderArr[i];
@@ -236,7 +236,7 @@ for(let i = 0;i <= 4;i++){
 //app_alert_check
 async function app_alert_check(){
 let new_time = Math.round(new Date().getTime()/1000).toString();
-var hsheader = hsheader.replace(/X-Khronos":"\d{10}/,`X-Khronos":"${new_time}`)
+hsheader = hsheader.replace(/X-Khronos":"\d{10}/,`X-Khronos":"${new_time}`)
 let iid = hsurl.match(/iid=\d+/)
 let idfa = hsurl.match(/idfa=\d+-\d+-\w+-\w+-\w+/)
 let vid = hsurl.match(/vid=\w+-\w+-\w+-\w+-\w+/)
